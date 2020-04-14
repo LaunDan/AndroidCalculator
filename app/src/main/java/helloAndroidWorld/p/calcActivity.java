2b8bfb160864aa58fcb8b131a4ef039c1e0be9b0
LaunDan <launadaniel2@gmail.com>
2020-04-14 21:50:55 +0200

    create TextViews for mainframe

--- app/src/main/java/helloAndroidWorld/p/calcActivity.java
+++ app/src/main/java/helloAndroidWorld/p/calcActivity.java
@@ -1,4 +1,18 @@
 package helloAndroidWorld.p;
 
-public class calcActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class calcActivity extends AppCompatActivity {
+    private TextView picture;
+    private TextView number1;
+    private TextView number2;
+    private TextView operation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        
+    }
 }
