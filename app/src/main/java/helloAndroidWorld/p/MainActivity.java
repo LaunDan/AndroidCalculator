4e7fcb016030e48790d21e36b3bfc13f8155daaa
LaunDan <launadaniel2@gmail.com>
2020-04-19 12:01:51 +0200

    hide status bar and activitynotification bar

--- app/src/main/java/helloAndroidWorld/p/MainActivity.java
+++ app/src/main/java/helloAndroidWorld/p/MainActivity.java
@@ -25,13 +25,23 @@ public class MainActivity extends AppCompatActivity {
     private double res;
     private int method = 0;
 
+    private View decorView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         screen = findViewById(R.id.resultText);
         screen.setText("0");
