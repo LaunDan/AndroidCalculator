339529e606ff418d971b1b2d70ce5d9b61683f0f
LaunDan <launadaniel2@gmail.com>
2020-04-19 17:18:56 +0200

    set wellcome frame

--- app/src/main/java/helloAndroidWorld/p/WelcomeActivity.java
+++ app/src/main/java/helloAndroidWorld/p/WelcomeActivity.java
@@ -8,11 +8,11 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Welcome extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layou);
 
 
 
