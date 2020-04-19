e21613ee700f1b177989e3ff2eff284d5e9818f9
LaunDan <launadaniel2@gmail.com>
2020-04-19 17:08:17 +0200

    set welcome frame

--- app/src/main/java/helloAndroidWorld/p/Welcome.java
+++ app/src/main/java/helloAndroidWorld/p/Welcome.java
@@ -1,4 +1,37 @@
 package helloAndroidWorld.p;
 
-public class Welcome {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Welcome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        ImageView imgvwSpsoa = findViewById(R.id.imageView2);
+        Animation animSpsoaLogo = AnimationUtils.loadAnimation(this, R.anim.weclome_sc);
+
+        imgvwSpsoa.startAnimation(animSpsoaLogo);
+        Thread thrdWlcmscrnDelay = new Thread() {
+            public void run() {
+                try {
+                    sleep(3500);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        thrdWlcmscrnDelay.start();
+    }
 }
