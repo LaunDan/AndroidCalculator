d6e2a2527e580ce204868bb48278e3a8a1763ec8
LaunDan <launadaniel2@gmail.com>
2020-04-20 15:20:36 +0200

    hide the tsk bar

--- app/src/main/java/helloAndroidWorld/p/WelcomeActivity.java
+++ app/src/main/java/helloAndroidWorld/p/WelcomeActivity.java
@@ -3,6 +3,7 @@ package helloAndroidWorld.p;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -10,11 +11,22 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         ImageView imgvwSpsoa = findViewById(R.id.imageView2);
         Animation animSpsoaLogo = AnimationUtils.loadAnimation(this, R.anim.weclome_sc);
