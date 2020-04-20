fcc7ac0556c3efda80f7047d382e247d61f0051e
LaunDan <launadaniel2@gmail.com>
2020-04-20 15:55:54 +0200

    timing animation

--- app/src/main/java/helloAndroidWorld/p/WelcomeActivity.java
+++ app/src/main/java/helloAndroidWorld/p/WelcomeActivity.java
@@ -37,7 +37,7 @@ public class WelcomeActivity extends AppCompatActivity {
         Thread thrdWlcmscrnDelay = new Thread() {
             public void run() {
                 try {
-                    sleep(7000);
+                    sleep(6000);
                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
