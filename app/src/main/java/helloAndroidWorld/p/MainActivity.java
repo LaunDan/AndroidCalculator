2fe25ca3f657c3fef1b54ceaab02ea8832bbaa56
LaunDan <launadaniel2@gmail.com>
2020-04-29 22:45:15 +0200

    solved problem with action bar and notification bar

--- app/src/main/java/helloAndroidWorld/p/MainActivity.java
+++ app/src/main/java/helloAndroidWorld/p/MainActivity.java
@@ -3,6 +3,7 @@ package helloAndroidWorld.p;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,23 +26,17 @@ public class MainActivity extends AppCompatActivity {
     private double res;
     private int method = 0;
 
-    private View decorView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
 
-        decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
 
         screen = findViewById(R.id.resultText);
         screen.setText("0");
